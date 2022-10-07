a31b5d2f1a4597bbbb89d58fbada402b35ed978d
Immanuel Victor <immanuelvictor.a.f@gmail.com>
2022-10-07 09:51:34 -0300

    env: adiciona estrutura base do projeto

---
+++ src/components/addMovie/index.jsx
@@ -0,0 +1,14 @@
+const AddMovie = () => {
+  return (
+    <form>
+      <div>
+        <label>Titulo:</label>
+        <input type="text" placeholder="Insira o título do filme"></input>
+      </div>
+      <div>
+        <label>Sinopse</label>
+        <input type="text" placeholder="Insira a sinopse do filme"></input>
+      </div>
+    </form>
+  );
+};
---
+++ src/components/movieItem/index.jsx
@@ -0,0 +1,12 @@
+const MovieItem = ({ props }) => {
+  return (
+    <div>
+      <header>
+        <h2>Titulo</h2>
+      </header>
+      <p>Sinopse:</p>
+    </div>
+  );
+};
+
+export { MovieItem };
